fd814aeafd0025d16fabc6dd1c1fb767aea465b7
Asmoodeeuus <cobongandrei07@gmail.com>
2025-02-27 17:03:15 +0800

    Updated warehouseMan route and database session file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const users = require('./routes/users.js');
+const warehousemanRoutes = require('./routes/warehouseMan.js');
 
 const app = express();
 
@@ -8,8 +9,8 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 app.use('/users', users);
+app.use("/warehouseman", warehousemanRoutes); 
 
-app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 3002;
