c1e2ca1b53b6eb1ecc0674167260f5640ec2ea3b
batsuburgeru <batoalexander10@gmail.com>
2025-04-16 15:40:47 +0800

    change date format to mm/dd/yyyy

--- routes/sapSyncLogs.js
+++ routes/sapSyncLogs.js
@@ -185,8 +185,8 @@ app.post(
 );
 
 const parseDate = (dateStr) => {
-  // Expecting format: dd/mm/yyyy
-  const [day, month, year] = dateStr.split("/").map(Number);
+  // Expecting format: mm/dd/yyyy
+  const [month, day, year] = dateStr.split("/").map(Number);
   return new Date(year, month - 1, day); // month is 0-indexed
 };
 
